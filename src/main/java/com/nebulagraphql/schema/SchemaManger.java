6fb40136b5964ab7e0a2449a37ae8fa82259ff2a
chuhuilong <dr.dragonchu@gmail.com>
2023-04-21 16:54:08 +0000

    refact schema manager

--- src/main/java/com/nebulagraphql/schema/SchemaManger.java
+++ src/main/java/com/nebulagraphql/schema/SchemaManger.java
@@ -23,11 +23,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class SchemaManger {
     private static final Logger logger = LoggerFactory.getLogger(SchemaManger.class);
 
     private final MetaClient metaClient;
+
+    private GraphQLObjectType.Builder query = GraphQLObjectType.newObject();
+
+    private GraphQLCodeRegistry.Builder codeRegistry = GraphQLCodeRegistry.newCodeRegistry();
 
     public SchemaManger(List<HostAddress> addresses) throws UnknownHostException {
         this.metaClient = new MetaClient(addresses, 30000, 3, 3);
@@ -35,70 +41,18 @@ public class SchemaManger {
 
     public GraphQLSchema generateSchema(String space, SessionPool sessionPool, MetaData metaData) {
         logger.debug("Generating graphql schema from space: {}", space);
+        DataFetcher<Object> propertyDataFetcher = new NebulaDataFetcher(sessionPool, metaData);
         try {
             metaClient.connect();
             List<TagItem> tags = metaClient.getTags(space);
-            GraphQLObjectType.Builder queryTypeBuilder = GraphQLObjectType.newObject();
-            GraphQLCodeRegistry.Builder codeRegistryBuilder = GraphQLCodeRegistry.newCodeRegistry();
-            DataFetcher<Object> propertyDataFetcher = new NebulaDataFetcher(sessionPool, metaData);
-            queryTypeBuilder.name("Query");
-            Map<String, Map<String, PropertyType>> tagsFieldsMap = new HashMap<>();
+            query.name("Query");
             for (TagItem tag : tags) {
-                GraphQLObjectType.Builder tagTypeBuilder = GraphQLObjectType.newObject();
-                String tagName = new String(tag.getTag_name(), StandardCharsets.UTF_8);
-                logger.debug("Generating schema for tag: {}", tagName);
-                tagTypeBuilder.name(tagName);
-                Schema schema = tag.getSchema();
-                List<GraphQLArgument> arguments = new ArrayList<>();
-                Map<String, PropertyType> fieldsMap = new HashMap<>();
-                for (ColumnDef columnDef : schema.getColumns()) {
-                    GraphQLFieldDefinition.Builder fieldDefinitionBuilder = GraphQLFieldDefinition.newFieldDefinition();
-                    String fieldName = new String(columnDef.getName(), StandardCharsets.UTF_8);
-                    GraphQLScalarType scalarType = SchemaUtils.getType(columnDef.type.getType());
-                    fieldsMap.put(fieldName, columnDef.type.getType());
-                    GraphQLArgument.Builder argumentBuilder = GraphQLArgument.newArgument();
-                    argumentBuilder.name(fieldName).type(scalarType).defaultValueLiteral(NullValue.of());
-                    byte[] desc = columnDef.getComment();
-                    if (desc != null) {
-                        argumentBuilder.description(new String(desc, StandardCharsets.UTF_8));
-                    }
-                    GraphQLArgument argument = argumentBuilder.build();
-                    arguments.add(argument);
 
-                    fieldDefinitionBuilder.name(fieldName)
-                            .type(scalarType);
-                    if (desc != null) {
-                        fieldDefinitionBuilder.description(new String(desc, StandardCharsets.UTF_8));
-                    }
-                    tagTypeBuilder.field(fieldDefinitionBuilder);
-                }
-                tagsFieldsMap.put(tagName, fieldsMap);
-                GraphQLObjectType tagType = tagTypeBuilder.build();
-
-                //add query for vertices according to properties
-                queryTypeBuilder.field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name(tagName + "s")
-                        .type(GraphQLNonNull.nonNull(GraphQLList.list(tagType)))// if there is no matching vertex, return empty list
-                        .arguments(arguments)
-                        .build());
-                //add query for specific vertex according to VID
-                queryTypeBuilder.field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name(tagName)
-                        .type(tagType)// result will be null if VID not exist
-                        .argument(GraphQLArgument.newArgument()
-                                .name("ID")
-                                .type(Scalars.GraphQLID)
-                                .description("Vertex ID")
-                                .build())
-                        .build());
-                codeRegistryBuilder.dataFetcher(FieldCoordinates.coordinates("Query", tagName + "s"), propertyDataFetcher);
-                logger.debug("Generate tag schema success, tagName: {}", tagName);
             }
-            GraphQLCodeRegistry codeRegistry = codeRegistryBuilder.build();
-            GraphQLObjectType queryType = queryTypeBuilder.build();
+            GraphQLObjectType queryType = query.build();
             GraphQLSchema graphQLSchema = GraphQLSchema.newSchema()
                     .query(queryType)
-                    .codeRegistry(codeRegistry)
+                    .codeRegistry(codeRegistry.build())
                     .build();
             logger.debug("Generate graphql schema from space success, space name: {}", space);
             return graphQLSchema;
@@ -109,5 +63,67 @@ public class SchemaManger {
         } finally {
             metaClient.close();
         }
+    }
+
+    public void generateTagSchema(TagItem tag,DataFetcher<Object> propertyDataFetcher){
+        String tagName = new String(tag.getTag_name(), StandardCharsets.UTF_8);
+        logger.debug("Generating schema for tag: {}", tagName);
+        GraphQLObjectType tagType = generateTagType(tag);
+        List<GraphQLArgument> arguments = generateArguments(tag);
+        //add query for vertices according to properties
+        query.field(GraphQLFieldDefinition.newFieldDefinition()
+                .name(tagName + "s")
+                .type(GraphQLNonNull.nonNull(GraphQLList.list(tagType)))// if there is no matching vertex, return empty list
+                .arguments(arguments)
+                .build());
+        //add query for specific vertex according to VID
+        query.field(GraphQLFieldDefinition.newFieldDefinition()
+                .name(tagName)
+                .type(tagType)// result will be null if VID not exist
+                .argument(GraphQLArgument.newArgument()
+                        .name("ID")
+                        .type(Scalars.GraphQLID)
+                        .description("Vertex ID")
+                        .build())
+                .build());
+        codeRegistry.dataFetcher(FieldCoordinates.coordinates("Query", tagName + "s"), propertyDataFetcher);
+        logger.debug("Generate tag schema success, tagName: {}", tagName);
+    }
+
+    
+    public GraphQLObjectType generateTagType(TagItem tag){
+        Schema schema = tag.getSchema();
+        String tagName = new String(tag.getTag_name(), StandardCharsets.UTF_8);
+        GraphQLObjectType.Builder tagTypeBuilder = GraphQLObjectType.newObject();
+        tagTypeBuilder.name(tagName);
+        for (ColumnDef columnDef : schema.getColumns()) {
+            GraphQLFieldDefinition.Builder fieldDefinitionBuilder = GraphQLFieldDefinition.newFieldDefinition();
+            String fieldName = new String(columnDef.getName(), StandardCharsets.UTF_8);
+            GraphQLScalarType scalarType = SchemaUtils.getType(columnDef.type.getType());
+            fieldDefinitionBuilder.name(fieldName)
+                    .type(scalarType);
+            byte[] desc = columnDef.getComment();
+            if (desc != null) {
+                fieldDefinitionBuilder.description(new String(desc, StandardCharsets.UTF_8));
+            }
+            tagTypeBuilder.field(fieldDefinitionBuilder);
+        }
+        return tagTypeBuilder.build();
+    }
+
+    public List<GraphQLArgument> generateArguments(TagItem tag){
+        return tag.schema.columns.stream().map(this::generateArgument).collect(Collectors.toList());
+    }
+
+    public GraphQLArgument generateArgument(ColumnDef columnDef){
+        String fieldName = new String(columnDef.getName(), StandardCharsets.UTF_8);
+        GraphQLScalarType scalarType = SchemaUtils.getType(columnDef.type.getType());
+        GraphQLArgument.Builder argumentBuilder = GraphQLArgument.newArgument();
+        argumentBuilder.name(fieldName).type(scalarType).defaultValueLiteral(NullValue.of());
+        byte[] desc = columnDef.getComment();
+        if (desc != null) {
+            argumentBuilder.description(new String(desc, StandardCharsets.UTF_8));
+        }
+        return argumentBuilder.build();
     }
 }
